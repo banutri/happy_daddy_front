9620e18fb1b912651cd4346c4d449b409dc31e8c
XCHROMAX <banutri@xchromax.localdomain>
2021-09-17 15:47:14 +0700

    save progress products card

--- vendor/materialize/js/init.js
+++ vendor/materialize/js/init.js
@@ -2,7 +2,23 @@
   $(function(){
 
     $('.sidenav').sidenav();
-    $('.parallax').parallax();
+    
+
+    $('.carousel.carousel-slider').carousel({
+      indicators:true,
+      fullWidth:true,
+      dist:-100,
+      padding:0,
+      
+    });
+
+    
+
+    setInterval(function(){
+      $('.carousel.carousel-slider').carousel('next');
+      console.log('ilih');
+    },5000);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
