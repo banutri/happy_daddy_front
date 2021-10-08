fd9df85be99c49f5e7f6f05d99514a8393f77048
banutri <banutriyantoko@gmail.com>
2021-10-08 11:20:35 +0700

    save progress

--- vendor/materialize/js/init.js
+++ vendor/materialize/js/init.js
@@ -1,37 +1,66 @@
-(function($){
-  $(function(){
+$(document).ready(function () {
 
-    $('.sidenav').sidenav();
+// Search bar
+  $('#modalSearch').modal({
+    dismissible: false,
+    onOpenEnd:function(){
+      $('input[name=searchQuery]').focus();
+    },
+    onCloseEnd:function(){
+      $('input[name=searchQuery]').val("");
+    },
     
-    $('.modal').modal({
-      dismissible:false,
-    });
+  });
+
+  $('#searchBarForm').on('submit',function(event){
+    event.preventDefault();
+    let searchQuery = $('input[name=searchQuery]').val();
+    console.log(searchQuery);
+  })
+// End search bar
+
+// Carousel
+  $('.carousel.carousel-slider').carousel({
+    indicators: true,
+    fullWidth: true,
+    dist: -100,
+    padding: 0,
+
+  });
+
+  setInterval(function () {
+    $('.carousel.carousel-slider').carousel('next');
+    console.log('ilih');
+  }, 5000);
+
+// End Carousel
+
+// Add to Cart
+  $('.add-to-cart').on('click', function () {
+    let productId = $(this).data('product-id');
+    console.log('keranjang id barang '+productId);
+  });
+// End Add to Cart
+
+// Product Click
+  $('.card-product').on('click', function () {
+    let productId = $(this).data('product-id');
+    console.log('Id barang = '+productId);
+  });
+// End Product click
 
 
-    $('.carousel.carousel-slider').carousel({
-      indicators:true,
-      fullWidth:true,
-      dist:-100,
-      padding:0,
-      
-    });
-
-    
-
-    setInterval(function(){
-      $('.carousel.carousel-slider').carousel('next');
-      console.log('ilih');
-    },5000);
 
 
-    $('.cart-btn').on('click',function(){
-      console.log('keranjang');
-    });
+// ========== Init Functions ============ //
 
-    $('.product-click').on('click',function(){
-      console.log('produk');
-    });
+// Sidenavabr
+$('.sidenav').sidenav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
 
+// Dropdown
+$(".dropdown-trigger").dropdown();
+
+// ========== End Init Functions ========= //
+
+});
